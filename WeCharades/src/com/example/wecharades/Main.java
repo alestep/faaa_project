aa521b9ed81600b150b88128c21085afc5f44849
Felix Karlsson <felixkarlsson@dhcp-196074.eduroam.chalmers.se>
2013-05-15 14:22:08 +0200

    Working login/registration system

--- WeCharades/src/com/example/wecharades/Main.java
+++ WeCharades/src/com/example/wecharades/Main.java
@@ -1,12 +1,8 @@
 package com.example.wecharades;
 
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class Main extends Activity {
 
@@ -14,24 +10,5 @@ public class Main extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-        Button b1 = (Button) findViewById(R.id.button1);
-        
-        b1.setOnClickListener(new OnClickListener(){
-		
-        	@Override
-        	public void onClick(View w) {
-			Intent intent = new Intent(Main.this, VideoCapture.class);
-			startActivity(intent);
-        	}
-        });
-    }
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
-
 }
