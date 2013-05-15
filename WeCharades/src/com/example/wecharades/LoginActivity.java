d46db1175297378ab0b212efd6fc371c6b3f5d57
Felix Karlsson <felixkarlsson@dhcp-196074.eduroam.chalmers.se>
2013-05-15 16:46:20 +0200

    Updated user database files

---
+++ WeCharades/src/com/example/wecharades/LoginActivity.java
@@ -0,0 +1,103 @@
+package com.example.wecharades;
+
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+//import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.ParseException;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+//import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+	Button btnLogin;
+	Button btnLinkToRegister;
+	Button btnForgotPassword;
+	EditText inputUsername;
+	EditText inputPassword;
+	TextView loginErrorMsg;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+		//Parse Stuff - Copy and Paste this into every onCreate method to be able to use Parse
+		Parse.initialize(this, "p34ynPRwEsGIJ29jmkGbcp0ywqx9fgfpzOTjwqRF", "RZpVAX3oaJcZqTmTwLvowHotdDKjwsi6kXb4HJ0R");
+
+		// Importing all assets like buttons, text fields
+		inputUsername		= (EditText) findViewById(R.id.loginUsername);
+		inputPassword		= (EditText) findViewById(R.id.loginPassword);
+		btnLogin			= (Button) findViewById(R.id.btnLogin);
+		btnLinkToRegister	= (Button) findViewById(R.id.btnLinkToRegisterScreen);
+		btnForgotPassword	= (Button) findViewById(R.id.btnForgotPassword);
+		loginErrorMsg		= (TextView) findViewById(R.id.login_error);
+
+		// Login button Click Event
+		btnLogin.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				//Using lowercase at login and registration to avoid case sensitivity problems
+				String username = inputUsername.getText().toString().toLowerCase();
+				//Should be case sensitive
+				String password = inputPassword.getText().toString();
+
+				/** 
+				 * TODO: Be able to login using e-mail?
+				 * 		 Probably using getUsername() and parse the username  
+				 **/
+				ParseUser.logInInBackground(username, password, new LogInCallback() {
+					@Override
+					public void done(ParseUser user, ParseException e) {
+						if (user != null) {
+							// Successful login! - Launch dashboard screen
+							Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+
+							// Close all views before launching Dashboard
+							dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+							startActivity(dashboard);
+
+							// Close Login Screen
+							finish();
+						} else {
+							// Signup failed. Look at the ParseException to see what happened. - Error code submitted until all bugs fixed
+							loginErrorMsg.setText("Incorrect username, e-mail or password" + e.getCode());
+						}
+					}
+				});
+			}
+		});
+
+
+		// Link to Register Screen
+		btnLinkToRegister.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent i = new Intent(getApplicationContext(),
+						RegisterActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+
+		// Link to Forgot Password
+		btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent i = new Intent(getApplicationContext(),
+						ResetPasswordActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+	}
+}
---
+++ WeCharades/src/com/example/wecharades/RegisterActivity.java
@@ -0,0 +1,121 @@
+package com.example.wecharades;
+
+import com.parse.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity {
+	Button btnRegister;
+	Button btnLinkToLogin;
+	EditText inputNickname;
+	EditText inputEmail;
+	EditText inputPassword;
+	EditText inputRepeatPassword;
+	TextView registerErrorMsg;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+
+		//Parse Stuff - Copy and Paste this into every onCreate method to be able to use Parse
+		Parse.initialize(this, "p34ynPRwEsGIJ29jmkGbcp0ywqx9fgfpzOTjwqRF", "RZpVAX3oaJcZqTmTwLvowHotdDKjwsi6kXb4HJ0R");
+
+		// Importing all assets like buttons, text fields
+		inputNickname = (EditText) findViewById(R.id.registerName);
+		inputEmail = (EditText) findViewById(R.id.registerEmail);
+		inputPassword = (EditText) findViewById(R.id.registerPassword);
+		inputRepeatPassword = (EditText) findViewById(R.id.registerRepeatPassword);
+		btnRegister = (Button) findViewById(R.id.btnRegister);
+		btnLinkToLogin = (Button) findViewById(R.id.btnLinkToLoginScreen);
+		registerErrorMsg = (TextView) findViewById(R.id.register_error);
+
+		// Register Button Click event
+		btnRegister.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View view) {
+
+				//To avoid case sensitivity problems...
+				String name = inputNickname.getText().toString().toLowerCase(); 
+				String email = inputEmail.getText().toString().toLowerCase();
+				//Should be case sensitive
+				String password = inputPassword.getText().toString();
+				String repeatPassword = inputRepeatPassword.getText().toString();
+
+				//My own validation check...
+				//TODO Check if it is possible to compare the two password inputs as an ParseException?
+				if(name == null || name.length() == 0) {
+					registerErrorMsg.setText("Please enter a valid name");
+				} else if (badPassword(password)) {
+					registerErrorMsg.setText("Please enter a valid password of minimum 6 characters");
+				} else if (!password.equals(repeatPassword)) {
+					registerErrorMsg.setText("You password doesn't match. Please try again!");
+				}  else {
+
+					ParseUser user = new ParseUser();
+					user.setUsername(name);
+					user.setPassword(password);
+					user.setEmail(email);
+					// other fields can be set just like with ParseObject (if you use setX you don't need put)
+					// for example: user.put("phone", "0706437341");
+					user.signUpInBackground(new SignUpCallback() {
+						@Override
+						public void done(ParseException e) {
+							if (e == null) {
+								// Successful registration - Launch Dashboard Screen
+								Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+								// Close all views before launching Dashboard
+								dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+								startActivity(dashboard);
+								// Close Registration Screen
+								finish();
+							} else {
+								// Sign up didn't succeed. Looking at ParseExceptions...
+								// Found a list of exceptions: https://www.parse.com/docs/android/api/constant-values.html
+								if (e.getCode() == 202) {
+									registerErrorMsg.setText("The username is already in use");
+								} else if (e.getCode() == 203) {
+									registerErrorMsg.setText("The email is already registered");
+								} else if (e.getCode() == 125) {
+									registerErrorMsg.setText("Please enter a valid e-mail");
+								} else {
+									//Unknown error - Error code until all problems fixed...
+									registerErrorMsg.setText("Oops! Something went wrong. Please try again." + e.getCode());
+								}
+							}
+						}
+					});
+				}
+			}
+		});
+
+		// Link to Login Screen
+		btnLinkToLogin.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+				startActivity(i);
+				// Close Registration View
+				finish();
+			}
+		});
+	}
+
+/* Not used atm - might be used later
+ * 	private static final Pattern goodEmailFormat = Pattern.compile(
+			"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+			);
+*/
+
+	private boolean badPassword(String password) {
+		//Only two criterias for now...
+		//TODO Fix stronger restrictions??
+		return (password == null || password.length() < 5);
+	}
+}
