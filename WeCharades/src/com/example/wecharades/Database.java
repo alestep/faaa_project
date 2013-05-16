9a1af6015659eadf6dedd1773b3e9c9aae78389b
Atom058 <anton.dahlstrom508@gmail.com>
2013-05-16 08:10:06 +0200

    Changed in database.java

--- WeCharades/src/com/example/wecharades/Database.java
+++ WeCharades/src/com/example/wecharades/Database.java
@@ -1,10 +1,11 @@
-package com.example.wecharades;
+kpackage com.example.wecharades;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -16,11 +17,13 @@ import com.parse.ParseQuery;
  *
  */
 public class Database {
-	
+	//Fix for strange query behavious
+	private static Object queryReturn;
+
 	private static String getWord(){
 		return "Testord"; //TODO This should be implemented later.
 	}
-	
+
 	/**
 	 * Creates a new game on the server
 	 * 
@@ -29,16 +32,16 @@ public class Database {
 	 */
 	public static void createGame(String playerId1, String playerId2) {
 		LinkedList<ParseObject> parseList = new LinkedList<ParseObject>();
-		
+
 		ParseObject newGame = new ParseObject("Game");
 		newGame.put("player1", playerId1);
 		newGame.put("player2", playerId2);
 		newGame.put("currentPlayer", playerId1);
 		newGame.put("turn", 1);
 		newGame.put("finished", false);
-		
+
 		parseList.add(newGame);
-		
+
 		//Adds all the six turns
 		ParseObject newTurn;
 		for(int i=1; i <= 6 ; i++){
@@ -53,15 +56,9 @@ public class Database {
 			newTurn.add("answerPlayer",playerId2);
 			newTurn.add("answerPlayerScore",0);
 		}
-		
-		try {
-			ParseObject.saveAll(parseList);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ParseObject.saveAllInBackground(parseList);
 	}
-	
+
 	//A private method to parse a ParseObject to a game
 	private static Game parseGame(ParseObject game){
 		if(game.getClassName().equals("Game")){
@@ -70,22 +67,28 @@ public class Database {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * A method to get a single game
 	 * @param gameId
 	 * @return
 	 */
 	public static Game getGame(String gameId){
-		try {
-			 return parseGame((new ParseQuery("Game")).get(gameId));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Game game;
+		ParseQuery query = new ParseQuery("Game");
+		query.getInBackground(gameId, new GetCallback() {
+			public void done(ParseObject object, ParseException e){
+				if(e == null){
+					queryReturn = parseGame(object);
+				} else{
+					//TODO Generate error code
+				}
+			}
+		});
+		return (Game) queryReturn;
 		return null;
 	}
-	
+
 	/**
 	 * Get a list of game-instances of the logged in player from the Parse server.
 	 * 
@@ -98,26 +101,26 @@ public class Database {
 		query.whereContains("player2", sharedPreferences);//TODO
 		return null;
 	}
-	
+
 	/**
 	 * Updates a game to the Parse server.
 	 * 
 	 */
 	public static void updateGame(Game theGame) {
-		
+
 	}
 
 	public static Player getPlayer(String playerId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/**
 	 * Puts the playerId into the the random queue
 	 */
 	public static void putIntoPlayerQueue(String playerId) {
 		//We should maybe check if the player is already in queue
-		
+
 		ParseObject queue = new ParseObject("RandomQueue");
 		queue.put("player", playerId);
 		queue.saveInBackground();
@@ -140,7 +143,7 @@ public class Database {
 	 */
 	public static ArrayList<String> getInvitations(String playerId) {
 		ArrayList<String> returnList = new ArrayList<String>();
-		
+
 		ParseQuery query = new ParseQuery("Invite");
 		query.whereContains("invitee", playerId);
 		try {
