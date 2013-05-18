afcda88f5f4a8b479dfe0c4d4845bd65f19b9f1b
Felix Karlsson <felixkarlsson@dhcp-198217.eduroam.chalmers.se>
2013-05-18 16:11:00 +0200

    adding resetpresenter.java

---
+++ WeCharades/src/com/example/wecharades/presenter/ResetPresenter.java
@@ -0,0 +1,13 @@
+package com.example.wecharades.presenter;
+
+import android.app.Activity;
+
+public class ResetPresenter extends Presenter {
+	
+	private Activity activity;
+	public ResetPresenter(Activity activity) {
+		super(activity);
+		this.activity = activity;
+	}
+	
+}
