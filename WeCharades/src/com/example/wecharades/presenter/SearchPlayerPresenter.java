f4906c7bbf5be30e78b72e0df7fe7ba7ddc67059
Alexander <alex.universitet@hotmail.co.uk>
2013-05-20 11:54:31 +0200

    update search 2

---
+++ WeCharades/src/com/example/wecharades/presenter/SearchPlayerPresenter.java
@@ -0,0 +1,29 @@
+package com.example.wecharades.presenter;
+
+import java.util.ArrayList;
+import android.app.Activity;
+
+import com.example.wecharades.model.Player;
+import com.example.wecharades.views.SearchPlayerActivity;
+
+public class SearchPlayerPresenter extends Presenter {
+	
+	private SearchPlayerActivity activity;
+	ArrayList<Player> players;
+	
+	/**
+	 * 
+	 * @param activity
+	 */
+	public SearchPlayerPresenter(Activity activity) {
+		super(activity);
+		this.activity = (SearchPlayerActivity) activity;
+	}
+	
+	
+
+	public void performSearch(String searchString) {
+		
+	}
+
+}
