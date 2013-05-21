ab30d4de7df3df4556103d54946db78035cc5f71
Alexander <alex.universitet@hotmail.co.uk>
2013-05-21 14:00:21 +0200

    renamed activity

---
+++ WeCharades/src/com/example/wecharades/views/NewGameActivity.java
@@ -0,0 +1,40 @@
+package com.example.wecharades.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.wecharades.R;
+
+public class NewGameActivity extends Activity {
+	
+	public final static String TAG = "NewGameScreen";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_game);
+	}
+	
+	/**
+	 * Starts new game with random opponent
+	 * @param view
+	 */
+	public void onClickRandom(View view) {
+		Log.d(TAG, "Clicked Find random player");
+	}
+	
+	/**
+	 * Directs user to search screen
+	 * @param view
+	 */
+	public void onClickSearchPlayer(View view) {
+		Log.d(TAG, "Clicked Search for player");
+		Intent intent = new Intent (getApplicationContext(), SearchPlayerActivity.class);
+		startActivity(intent);
+	}
+}
