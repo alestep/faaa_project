005770b948bdf4e4acc9cb5d51a1b2c90b94a43e
Atom058 <anton.dahlstrom508@gmail.com>
2013-05-21 16:02:19 +0200

    Changed variable names

--- WeCharades/src/com/example/wecharades/GameAdapter.java
+++ WeCharades/src/com/example/wecharades/GameAdapter.java
@@ -13,13 +13,13 @@ import com.example.wecharades.model.Game;
 
 public class GameAdapter extends ArrayAdapter<Game> {
 
-	private ArrayList<Game> items;
+	private ArrayList<Game> games;
 	private LayoutInflater li;
 	private Context context;
 
 	public GameAdapter(Context context, ArrayList<Game> items) {
 		super(context, 0, items);
-		this.items = items;
+		this.games = items;
 		this.context = context;
 		li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
@@ -28,15 +28,15 @@ public class GameAdapter extends ArrayAdapter<Game> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View v = convertView;
 
-		final Game i = items.get(position);
-		if (i != null) {
+		final Game game = games.get(position);
+		if (game != null) {
 				v = li.inflate(R.layout.list_item_active_game, null);
 				final TextView title = (TextView) v.findViewById(R.id.opponent);
 				final TextView subtitle = (TextView) v.findViewById(R.id.score);
 				
 				if (title != null)
 					//TODO: Bestäm hur vi ska göra med detta. Skall vi skicka med username till denna klass eller ej?
-					title.setText( true ? i.getPlayerId2().getName() : i.getPlayerId1().getName()); 
+					title.setText( true ? game.getPlayerId2().getName() : game.getPlayerId1().getName()); 
 				if(subtitle != null)
 					//TODO: Undersök hur vi kan hämta score här.
 					subtitle.setText("The score is 5-6");
