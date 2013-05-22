d0f9a8ac9cba0f9b830bdd89994a84f4dcc3686c
Alexander <alex.universitet@hotmail.co.uk>
2013-05-22 16:59:08 +0200

    update fixed Presenter and GenericActivity

--- WeCharades/src/com/example/wecharades/views/SearchResultActivity.java
+++ WeCharades/src/com/example/wecharades/views/SearchResultActivity.java
@@ -1,21 +1,20 @@
 package com.example.wecharades.views;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.wecharades.R;
 import com.example.wecharades.presenter.SearchResultPresenter;
 
-public class SearchResultActivity extends ListActivity {
+public class SearchResultActivity extends GenericActivity {
 	SearchResultPresenter presenter;
-	
-	//TODO extending a listactivity causes a problem.. We CAN use a have-relation instead.
-	
 	/**
 	 *
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_screen);
 		presenter = new SearchResultPresenter(this);
 	}
 	
@@ -30,5 +29,11 @@ public class SearchResultActivity extends ListActivity {
 
 	public void invite(String invitee){
 		//presenter.invite(invitee);
+	}
+
+	@Override
+	public TextView getTextArea() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
