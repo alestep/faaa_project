91a22d39a35f9a0a2bab4f2285e2480de8988528
Alexander <alex.universitet@hotmail.co.uk>
2013-06-04 12:45:37 +0200

    updates design

--- WeCharades/src/com/example/wecharades/presenter/GameAdapter.java
+++ WeCharades/src/com/example/wecharades/presenter/GameAdapter.java
@@ -20,12 +20,12 @@ import com.example.wecharades.views.StartActivity;
  * @author weCharade
  */
 public class GameAdapter extends ArrayAdapter<Game> {
-	
+
 	private StartActivity activity;
 	private ArrayList<Game> games;
 	private Player user;
 	private Map<Game, Map<Player, Integer>> score;
-	
+
 	/**
 	 * Create an instance of GameAdapter
 	 * @param activity		The activity connected to the Adapter
@@ -34,7 +34,7 @@ public class GameAdapter extends ArrayAdapter<Game> {
 	 * @param score			Nested Map with Game as outer key and Map a value, with Player as key and score as value
 	 */
 	public GameAdapter(StartActivity activity, ArrayList<Game> games, Player user, Map<Game, Map<Player, Integer>> score) {
-		
+
 		//Do not provide any layout resource id to the super-class, hence the zero value.
 		super(activity, 0, games);
 		this.activity = activity;
@@ -42,38 +42,74 @@ public class GameAdapter extends ArrayAdapter<Game> {
 		this.games = games;
 		this.user = user;
 	}
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		
+
 		View v = convertView;
 		LayoutInflater li = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		final Game game = games.get(position);
-		
+
+
 		if (game != null) {
-			
-			v = li.inflate(R.layout.list_item_game, null);
-			final TextView title = (TextView) v.findViewById(R.id.opponent);
-			final TextView subtitle = (TextView) v.findViewById(R.id.score);
-			
+
 			//Check which of the players in the game who is the user in order to know which name to display
 			Player opponent = game.getPlayer1().equals(user) ? game.getPlayer2() : game.getPlayer1();
-			
-			if (title != null)
-				
-				title.setText(opponent.getName());
-			
-			if(subtitle != null) {
-				
-				int currentPlayerScore	= score.get(game).get(user);
-				int opponentScore		= score.get(game).get(opponent);
-				subtitle.setText("The score is " + currentPlayerScore + " - " + opponentScore);
-				
+
+			if (position == 0) {
+
+				v = li.inflate(R.layout.list_item_game_top, null);
+				final TextView title = (TextView) v.findViewById(R.id.opponent);
+				final TextView subtitle = (TextView) v.findViewById(R.id.score);
+
+				if (title != null)
+
+					title.setText(opponent.getName());
+
+				if(subtitle != null) {
+
+					int currentPlayerScore	= score.get(game).get(user);
+					int opponentScore		= score.get(game).get(opponent);
+					subtitle.setText("Score: " + currentPlayerScore + " - " + opponentScore);
+
+				}
+			} else if(position == games.size()-1) {
+				v = li.inflate(R.layout.list_item_game_bottom, null);
+				final TextView title = (TextView) v.findViewById(R.id.opponent);
+				final TextView subtitle = (TextView) v.findViewById(R.id.score);
+
+				if (title != null)
+
+					title.setText(opponent.getName());
+
+				if(subtitle != null) {
+
+					int currentPlayerScore	= score.get(game).get(user);
+					int opponentScore		= score.get(game).get(opponent);
+					subtitle.setText("Score: " + currentPlayerScore + " - " + opponentScore);
+
+				}
+			} else {
+				v = li.inflate(R.layout.list_item_game, null);
+				final TextView title = (TextView) v.findViewById(R.id.opponent);
+				final TextView subtitle = (TextView) v.findViewById(R.id.score);
+
+				if (title != null)
+
+					title.setText(opponent.getName());
+
+				if(subtitle != null) {
+
+					int currentPlayerScore	= score.get(game).get(user);
+					int opponentScore		= score.get(game).get(opponent);
+					subtitle.setText("Score: " + currentPlayerScore + " - " + opponentScore);
+
+				}
 			}
 		}
-		
+
 		return v;
-		
+
 	}
 }
 
